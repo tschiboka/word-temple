aea92339217a484a85d7674272eccd4f2057d17a
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2025-05-26 20:28:32 +0100

    Add palette and builder button

---
+++ src/common/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import './Button.scss'
+
+type ButtonProps = {
+    label: string
+    onClick: () => void
+}
+
+export const Button = ({ label, onClick }: ButtonProps) => {
+    return (
+        <button className="Button" onClick={onClick}>
+            {label}
+        </button>
+    )
+}
